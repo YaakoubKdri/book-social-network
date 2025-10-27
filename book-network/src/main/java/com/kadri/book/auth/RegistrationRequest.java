4d95fc9da86bc5da34a605078f678817bd3ecd57
kadri <kadriyaakoub@gmail.com>
2025-10-26 19:47:25 -0500

    Create the authentication controller

---
+++ book-network/src/main/java/com/kadri/book/auth/RegistrationRequest.java
@@ -0,0 +1,36 @@
+package com.kadri.book.auth;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+public class RegistrationRequest {
+
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String firstname;
+
+    @NotEmpty(message = "Lastname is mandatory")
+    @NotBlank(message = "Lastname is mandatory")
+    private String lastname;
+
+    @Email(message = "Email is not formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @NotEmpty(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    @Size(min = 8, message = "Password should be at least 8 characters")
+    private String password;
+}
