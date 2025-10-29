4472b9780040d44798508746abbb9088fa7b6a1d
kadri <kadriyaakoub@gmail.com>
2025-10-28 22:16:37 -0500

    Implement the Email sender service

--- book-network/src/main/java/com/kadri/book/auth/AuthenticationService.java
+++ book-network/src/main/java/com/kadri/book/auth/AuthenticationService.java
@@ -1,11 +1,15 @@
 package com.kadri.book.auth;
 
+import com.kadri.book.email.EmailService;
+import com.kadri.book.email.EmailTemplateName;
 import com.kadri.book.role.RoleRepository;
 import com.kadri.book.user.Token;
 import com.kadri.book.user.TokenRepository;
 import com.kadri.book.user.User;
 import com.kadri.book.user.UserRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +25,12 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final TokenRepository tokenRepository;
+    private final EmailService emailService;
 
-    public void register(RegistrationRequest request) {
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+    public void register(RegistrationRequest request) throws MessagingException {
         var userRole = roleRepository.findByName("USER")
                 .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
         var user = User.builder()
@@ -38,9 +46,17 @@ public class AuthenticationService {
         sendValidationEmail(user);
     }
 
-    private void sendValidationEmail(User user) {
+    private void sendValidationEmail(User user) throws MessagingException {
         var newToken = generateAndSaveActivationToken(user);
-        // todo send email
+        // send email
+        emailService.sendEmail(
+                user.getEmail(),
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "account activation"
+        );
     }
 
     private String generateAndSaveActivationToken(User user) {
